9604ac493e9b6f594306fc43cfb5758bfbf7ab25
chemawall <jmmuro88@gmail.com>
2022-12-04 08:35:47 +0000

    completado

--- src/models.py
+++ src/models.py
@@ -1,6 +1,6 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String, Bolean
+from sqlalchemy import Column, ForeignKey, Integer, String, Boolean,Text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import create_engine
@@ -14,63 +14,49 @@ class User(Base):
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    activity_id = Column(Integer)
+    activity_id = Column(Integer, ForeignKey('activity.id'))
     email = Column(String(80), nullable= False)
-    log = Column(Bolean)
-    followers_id = Column(Integer)
-    followings_id = Column(Integer)
+    log = Column(Boolean)
+    followers_id = Column(Integer, ForeignKey('followers.id'))
+    followings_id = Column(Integer, ForeignKey('followings.id'))
 
 class Activity(Base):
     __tablename__ = 'activity'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    coments_id = Column(Integer)
-    like_post_id = Column(Integer)
-    like_coments_id = Column(Integer)
-    post_id = Column(Integer)
+    coments_id = Column(Integer, ForeignKey('coments.id'))
+    like_post_id = Column(Integer, ForeignKey('likepost.id'))
+    like_coments_id = Column(Integer, ForeignKey('likecoment.id'))
+    post_id = Column(Integer,ForeignKey('post.id'))
 
 class Followings(Base):
     __tablename__ = 'followings'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(80), nullable=False)
 
 class Followers(Base):
     __tablename__ = 'followers'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    name = Column(String(80), nullable=False)
 
 class LikePost(Base):
     __tablename__ = 'likepost'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
-    post_id = (Integer)
+    post_id = (Integer, ForeignKey('post.id'))
 
 class LikeComent(Base):
-    __tablename__ = 'like_post'
-    # Here we define columns for the table person
-    # Notice that each column is also a normal Python instance attribute.
+    __tablename__ = 'likecoment'
     id = Column(Integer, primary_key=True)
-    like_coment = Column(Bolean)
-    post_id = (Integer)
+    like_coment = Column(Boolean)
+    post_id = (Integer,ForeignKey('post.id'))
 
 class Post(Base):
     __tablename__ = 'post'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
     img = Column(String(250), nullable=False)
-    text = Column(text)
+    text = Column(Text)
     number_like_post = Column(Integer)
-    coments_id = Column(Integer)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    coments_id = Column(Integer, ForeignKey('coments.id'))
 
     def to_dict(self):
         return {}
